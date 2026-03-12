03f17091a236d4fe18787e99fb52bf3d218a5ecc
dwykee <150531557+dwykee@users.noreply.github.com>
2026-03-12 13:47:09 +0700

    change
    Removed SSL options from database configuration.

--- config/database.php
+++ config/database.php
@@ -59,11 +59,6 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-                PDO::ATTR_TIMEOUT => 60,
-                PDO::MYSQL_ATTR_CONNECT_TIMEOUT => 60,
-            ]) : [],
         ],
 
         'mariadb' => [
